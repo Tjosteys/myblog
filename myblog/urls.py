766a0cb6c4c98c28c26c509debd60a2f4ed27ff2
tjosteys <lipedetope04@gmail.com>
2023-09-25 23:47:30 +0100

    third commit

--- myblog/urls.py
+++ myblog/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
+# from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="index.html"), name="index"),
+    # path("", TemplateView.as_view(template_name="index.html"), name="index"),
     path('admin/', admin.site.urls),
     path('accounts/', include("accounts.urls")),
     path('accounts/', include("django.contrib.auth.urls")),
     path('blog/', include("blog.urls")),
+    path('', include("pages.urls")),
 ]
